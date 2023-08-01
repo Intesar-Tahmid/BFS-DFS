baa4c23ada4a22f1b7d078513b276d3491c05e5f
Intesar-Tahmid <intesar3006@gmail.com>
2023-08-01 11:37:09 +0600

    new file add korlam kaj hoy kina

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a+b << endl;
+
+    cout << "Kire mamo" << endl;
+
+    return 0;
+}
